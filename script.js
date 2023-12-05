a1a58e401a20588423f4e058dcafd6d313104fc9
NIMCOO <nimco77001@gmail.com>
2023-12-05 07:46:54 +0000

    Done

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Good Luck 💪🏾
+function mostExpensive(dahab){
+    //code
+    let expensiveJewelry(){
+        
+    for(const jewelry of dahab){
+        console.log(jewelry),
+        if("Diamond Earrings">"Pearl Necklace")
+        console .log("The most expensive")
+    }
+}
+
+mostExpensive ({
+    "Diamond Earrings": 980,
+    "Gold Watch": 250,
+    "Pearl Necklace": 4650
+  }) ➞ "The most expensive one is the Pearl Necklace"
